177276d751a0e11a2cd7986ae92f08062d864659
Sammy1088 <szshreder@gmail.com>
2013-11-07 15:13:01 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gabysite::Application.config.secret_key_base = 'c0af29aa21aa2d91cd28541e81c5cc9c653e7691412a43bee252040c38286f631ef5d9489805cd0c8e518d5d427c8ffdbc3cca3aeb386be51354d43e9db3befa'
